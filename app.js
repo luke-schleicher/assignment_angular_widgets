abaef4d005c0566f294fce601e0c87d6061784b2
Alexa <alexa.anderson@outlook.com>
2017-01-20 11:04:54 -0600

    Add some $scope

--- app.js
+++ app.js
@@ -9,8 +9,18 @@ widgets.controller('RestaurantCtrl',
       var restaurant = {};
       restaurant.name = $scope.name;
       restaurant.foodType = $scope.typeOfFood;
-      restaurants.push(restaurant);
+      $scope.restaurants.push(restaurant);
       $scope.name = '';
       $scope.typeOfFood = '';
     }
+    $scope.deleteRestaurant = function(deletedRestaurant) {
+      for (var i = 0; i < $scope.restaurants.length; i++) {
+        var restaurant = $scope.restaurants[i];
+        if (deletedRestaurant.name === restaurant.name) {
+          $scope.restaurants.splice(i, 1);
+          break;
+        }
+      }
+    }
+
   }]);
